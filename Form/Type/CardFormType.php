ee820159136b7eec9678dcff755bcdfd920b2053
Léo Benoist <leo.benoist@gmail.com>
2014-09-29 11:19:02 -0400

    Improve month list

--- Form/Type/CardFormType.php
+++ Form/Type/CardFormType.php
@@ -19,7 +19,7 @@ class CardFormType extends AbstractType
                 array(
                     'required' => true,
                     'attr' => array('data-stripe' => 'exp-month'),
-                    'choices' => array('01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'),
+                    'choices' => array_combine(range(1, 12), range(1, 12)),
                 )
             )
             ->add(
