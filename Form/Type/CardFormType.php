cc1ccca5abdd9ee4bf32d9a51975668b5be8dc08
Léo Benoist <leo.benoist@gmail.com>
2014-10-08 10:31:52 -0400

    Form translation

--- Form/Type/CardFormType.php
+++ Form/Type/CardFormType.php
@@ -12,12 +12,19 @@ class CardFormType extends AbstractType
     {
         $builder
             ->add('card', 'text', array(
-                'required' => true, 
-                'label' => 'Card Number',
-                'attr' => array('data-stripe' => 'number'),
+                    'required' => true,
+                    'attr' => array('data-stripe' => 'number'),
+                    'label' => 'form.card.number',
+                    'translation_domain' => 'WMCStripeBundle',
                 )
             )
-            ->add('cvc', 'integer', array('required' => true, 'attr' => array('data-stripe' => 'cvc')))
+            ->add('cvc', 'integer', array(
+                    'required' => true,
+                    'attr' => array('data-stripe' => 'cvc'),
+                    'label' => 'form.card.cvc',
+                    'translation_domain' => 'WMCStripeBundle',
+                )
+            )
             ->add(
                 'month',
                 'choice',
@@ -25,6 +32,8 @@ class CardFormType extends AbstractType
                     'required' => true,
                     'attr' => array('data-stripe' => 'exp-month'),
                     'choices' => array_combine(range(1, 12), range(1, 12)),
+                    'label' => 'form.card.exp.month',
+                    'translation_domain' => 'WMCStripeBundle',
                 )
             )
             ->add(
@@ -34,6 +43,8 @@ class CardFormType extends AbstractType
                     'required' => true,
                     'attr' => array('data-stripe' => 'exp-year'),
                     'choices' => array_combine(range(date('Y'), date('Y') + 10), range(date('Y'), date('Y') + 10)),
+                    'label' => 'form.card.exp.year',
+                    'translation_domain' => 'WMCStripeBundle',
                 )
             )
             ->add('token', 'hidden');
